c560657fc67869f26c746eabfc691f2b231ef4b0
Mosiur12 <mrsojib9600@gmail.com>
2023-02-13 01:06:45 +0600

    first commit

---
+++ api-base/src/components/ApiList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ApiList = () => {
+    return (
+        <div>
+            <div className="card">
+                <h6>desk</h6>
+                <p>{}</p>
+            </div>
+            
+        </div>
+    );
+};
+
+export default ApiList;
